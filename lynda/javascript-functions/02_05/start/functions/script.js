5001635e0de0cd45da20968fa41f9c7aa1b2871f
Imanuel Gittens <gittensimanuel@gmail.com>
2015-06-22 06:50:12 -0400

    Revert "Lynda.com Javascript Functions: Exercises 1_3, 2_1, 2_2 and 2_3"
    This reverts commit 463c5dff7727b5fd40339b180f8a5f42e6163690.

--- lynda/javascript-functions/02_05/start/functions/script.js
+++
@@ -1,9 +0,0 @@
-var plus = function(){
-    var sum = 0;   
-    for(var i = arguments.length - 1; i >= 0; i--){
-        sum += arguments[i];   
-    }
-    return sum;
-}
-
-console.log(plus(2,2,2));
