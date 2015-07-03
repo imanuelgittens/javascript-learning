bff4bd5fbdeb354e6e7d60defedf4e3afe4f59bb
Imanuel Gittens <gittensimanuel@gmail.com>
2015-07-03 15:33:11 -0400

    Udemy: JS Weird Parts - ObjectsAndDot, ObjectLiterals and FakingNamespaces lessons

---
+++ JS Weird Parts/D3_FakingNamespaces/Starter/app.js
@@ -0,0 +1,17 @@
+var greet = 'Hello!';
+var greet = 'Hola!'; 
+
+console.log(greet);
+
+
+var english = {
+    greetings: {
+        basic: 'Hello';   
+    }
+};
+var spanish = {};
+
+english.greet = 'Hello!';
+spanish.greet = 'Hola!';
+
+console.log(english);
