67b31d899af5e3f904a69d1f7986469a3662d1fd
Imanuel Gittens <gittensimanuel@gmail.com>
2015-07-28 07:36:26 -0400

    Udemy: JS Weird parts - Closures pt2, function factories, closures and callback lessons.

---
+++ JS Weird Parts/D18_ClosuresAndCallbacks/Starter/app.js
@@ -0,0 +1,39 @@
+function sayHiLater(){
+    
+    var greeting = 'Hi';
+    
+    setTimeout(function(){
+        
+        console.log(greeting);  
+        
+    }, 3000);
+    
+}
+
+sayHiLater();
+
+
+//jQuery uses function expressions and first-class functions
+/*$('button').click(function(){
+    
+});*/
+
+
+
+
+function tellMeWhenDone(callback){
+
+    var a = 1000; //some work
+    var b = 2000; //some work
+    
+    callback(); //the 'callback', it runs the function I give it!
+    
+}
+
+tellMeWhenDone(function(){
+    alert('I\'m done!');
+});
+
+tellMeWhenDone(function(){
+    console.log('I\'m done!');
+});
